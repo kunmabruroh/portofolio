f0451642fc98b135edff689e40592d70336f2e24
kunmabruroh <kunmabruroh214@gmail.com>
2025-01-12 16:57:15 +0700

    merubah text main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings:["UI/UX Design" , "Web Designer" , "#"],
+    strings:["UI/UX Design" , "Website" , "Desain Grafis"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
